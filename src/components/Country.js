c7de4166e1b2313654d8082f537b23a0861bcf6f
enriquetanada <enriquetanada@gmail.com>
2020-09-29 13:46:37 +0800

    create partial for country card and fetch data from API

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,33 +1,37 @@
 import React, {useState, useEffect} from 'react'
+import CountryCard from './../partials/CountryCard'
 
 const Country = (props) => {
 	
+	const [countries, setCountries] = useState([])
+
+	useEffect(() =>{
+		fetch("https://restcountries.eu/rest/v2/all")
+		.then(res =>{return res.json()})
+		.then(countries =>{setCountries(countries)})
+	},[])
+
+	let countryList = countries.map(country =>(
+   		<div className="col-12 col-md-4 col-lg-3 mx-auto mt-5" key={country.name}>
+   			<CountryCard country={country} />
+   		</div>
+	))
   	return (
   	
     <div className="container mt-3">
-    	{/*search engine start*/}
+    	
     	<div className="row">
+    		{/*search engine start*/}
 	    	<form className="form-inline col-12 mx-auto">
 			  	<i className="fas fa-search" aria-hidden="true"></i>
 			  	<input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"
 			    aria-label="Search"/>
 			</form>
-		</div>
-    	{/*search engine end */}
-   		{/*display country start */}
-   		<div className="row">
-   			<div className="col-12 col-md-4 col-lg-3 mx-auto">
-   				<div className="card">
-				  	<img src="..." className="card-img-top" alt="..." />
-				  	<div className="card-body">
-				    	<h5 className="card-title">Card title</h5>
-				    	<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				    	<a href="#" className="btn btn-primary">Go somewhere</a>
-				  	</div>
-				</div>
-   			</div>
-   		</div>
-    	{/*display country end */}
+    		{/*search engine end */}
+    		{/*display country start */}
+			{countryList}
+    		{/*display country end */}
+		</div>	
     </div>
 
 
